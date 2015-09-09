02f38dbb630b1eb1f78f9b7fc06a27ed683e1da0
Paul Zapion Yang <zapionator@gmail.com>
2015-09-09 11:59:01 +0800

    update mtbf testcase

--- mtbf_driver/MtbfTestCase.py
+++ mtbf_driver/MtbfTestCase.py
@@ -5,6 +5,8 @@
 import time
 
 from gaiatest import GaiaTestCase
+from gaiatest import DEFAULT_SETTINGS
+from gaiatest import DEFAULT_PREFS
 from gaiatest.apps.homescreen.app import Homescreen
 from marionette_driver.by import By
 
@@ -59,6 +61,18 @@ class GaiaMtbfTestCase(GaiaTestCase):
         if not self.device.is_screen_enabled:
             self.device.turn_screen_on()
 
+        # restore prefs from testvars
+        default_prefs = DEFAULT_PREFS.copy()
+        default_prefs.update(self.testvars.get('prefs', {}))
+        default_prefs = self.modify_prefs(default_prefs)
+        for name, value in default_prefs.items():
+            if type(value) is int:
+                self.data_layer.set_int_pref(name, value)
+            elif type(value) is bool:
+                self.data_layer.set_bool_pref(name, value)
+            else:
+                self.data_layer.set_char_pref(name, value)
+
         # unlock
         if self.data_layer.get_setting('lockscreen.enabled'):
             self.device.unlock()
@@ -68,44 +82,26 @@ class GaiaMtbfTestCase(GaiaTestCase):
             self.apps.kill(self.apps.displayed_app)
 
         if full_reset:
-            # disable passcode
-            self.data_layer.set_setting('lockscreen.passcode-lock.code', '1111')
-            self.data_layer.set_setting('lockscreen.passcode-lock.enabled', False)
-
-            # change language back to english
-            self.data_layer.set_setting("language.current", "en-US")
-
-            # reset keyboard to default values
-            self.data_layer.set_setting("keyboard.enabled-layouts",
-                                        "{'app://keyboard.gaiamobile.org/manifest.webapp': {'en': True, 'number': True}}")
-
-            # reset do not track
-            self.data_layer.set_setting('privacy.donottrackheader.value', '-1')
-
-            # don't change status of airplane mode
-            # if self.data_layer.get_setting('airplaneMode.enabled'):
-            #    # enable the device radio, disable airplane mode
-            #    self.data_layer.set_setting('airplaneMode.enabled', False)
-
-            # Re-set edge gestures pref to False
-            self.data_layer.set_setting('edgesgesture.enabled', False)
+            # restore settings from testvars
+            default_settings = DEFAULT_SETTINGS.copy()
+            default_settings.update(self.testvars.get('settings', {}))
+            default_settings = self.modify_settings(default_settings)
+            for name, value in default_settings.items():
+                self.data_layer.set_setting(name, value)
 
             # disable carrier data connection
             if self.device.has_mobile_connection:
                 self.data_layer.disable_cell_data()
 
-            self.data_layer.disable_cell_roaming()
-
-            ## TODO: Disable wifi operation since Bug 1064800
-            # if self.device.has_wifi:
-            #     # Bug 908553 - B2G Emulator: support wifi emulation
-            #     if not self.device.is_emulator:
-            #         self.data_layer.enable_wifi()
-            #         self.data_layer.forget_all_networks()
-            #         self.data_layer.disable_wifi()
+            if self.device.has_wifi:
+                # Bug 908553 - B2G Emulator: support wifi emulation
+                if not self.device.is_emulator:
+                    self.data_layer.enable_wifi()
+                    self.data_layer.forget_all_networks()
+                    self.data_layer.disable_wifi()
 
             # don't remove contact data
-            # self.data_layer.remove_all_contacts()
+            self.data_layer.remove_all_contacts()
 
             # reset to home screen
             self.device.touch_home_button()
@@ -115,18 +111,6 @@ class GaiaMtbfTestCase(GaiaTestCase):
 
         # disable auto-correction of keyboard
         self.data_layer.set_setting('keyboard.autocorrect', False)
-
-        # restore settings from testvars
-        [self.data_layer.set_setting(name, value) for name, value in self.testvars.get('settings', {}).items()]
-
-        # restore prefs from testvars
-        for name, value in self.testvars.get('prefs', {}).items():
-            if type(value) is int:
-                self.data_layer.set_int_pref(name, value)
-            elif type(value) is bool:
-                self.data_layer.set_bool_pref(name, value)
-            else:
-                self.data_layer.set_char_pref(name, value)
 
     def tearDown(self):
         time.sleep(1)
